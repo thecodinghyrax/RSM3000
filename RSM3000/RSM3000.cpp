a7f25241f6d37f7389c836824f4f8ff0c973abd0
b-whisler <92903702+b-whisler@users.noreply.github.com>
2022-10-02 16:18:37 -0500

    More functionality for getSeatingRec

--- RSM3000/RSM3000.cpp
+++ RSM3000/RSM3000.cpp
@@ -15,12 +15,25 @@ int main()
 {
     string input;
     vector<SeatingGroup*> seating;
-    Booth b = Booth();
+
 
     // Default seating for testing
+    Booth b = Booth();
+    Booth b2 = Booth(204,4);
+    Booth b3 = Booth(205,5);
+    Bar r1 = Bar(300,2);
+    Bar r2 = Bar(301,3);
+    Table t1 = Table(400);
+    Table t2 = Table(401,6);
+    Table t3 = Table(410,8);
     seating.push_back(&b);
-    //seating.push_back(make_unique<Booth>(200));
-    //seating.push_back(make_unique<Booth>(205,4));
+    seating.push_back(&b2);
+    seating.push_back(&b3);
+    seating.push_back(&r1);
+    seating.push_back(&r2);
+    seating.push_back(&t1);
+    seating.push_back(&t2);
+    seating.push_back(&t3);
 
     cout << "Restaurant Seating Manager 3000 Menu" << endl;
 
@@ -35,6 +48,7 @@ int main()
             cout << "Enter the party size: ";
             cin >> input;
             //TODO: Validate input is an integer larger than 1
+            int partySize = stoi(input);
             cout << "Enter the preferred table type. T for table, B for booth, R for bar, or N for no preference: ";
             cin >> input;
             while (input != "t" && input != "T" && input != "b"
@@ -43,11 +57,23 @@ int main()
                 cout << "Invalid input. Please enter T for table, B for booth, R for bar, or N for no preference: ";
                 cin >> input;
             }
+            string prefType;
+            if (input == "t" || input == "T"){
+                prefType = "table";
+            } else if (input == "b" || input == "B"){
+                prefType = "booth";
+            } else if (input == "r" || input == "R"){
+                prefType = "bar";
+            } else if (input == "n" || input == "N"){
+                prefType = "np";
+            }
 
-            int tableIndex = getSeatingRec(seating, "booth", 6);
+
+            int tableIndex = getSeatingRec(seating, prefType, partySize);
             int tableNumber = seating[tableIndex]->getTableNumber();
+            string tableType = seating[tableIndex]->getSeatingType();
 
-            cout << "The party should be seated at table " << tableNumber << ". Enter Y to confirm, or N to manually select a table: ";
+            cout << "The party should be seated at " << tableType << " " << tableNumber << ". Enter Y to confirm, or N to manually select a table: ";
             cin >> input;
             while (input != "y" && input != "Y" && input != "n" && input != "N"){
                 cout << "Invalid input. Please enter Y to confirm table " << tableNumber << ", or N to manually select a table: ";
@@ -76,7 +102,32 @@ int getSeatingRec(vector<SeatingGroup*> seating, string seatingType, int partySi
         }
         count++;
     }
-
+    if (match > -1){
+        return match;
+    }
     // Second phase, checks for same type but larger capacity than party size
+    count = 0;
+    for (auto& s: seating){
+        if (!s->getOccupied() && s->getCapacity() > partySize && s->getSeatingType() == seatingType){
+            match = count;
+        }
+        count++;
+    }
+    if (match > -1){
+        return match;
+    }
+    // Third phase, find table of different type
+    count = 0;
+    for (auto& s: seating){
+        if (!s->getOccupied() && s->getCapacity() > partySize){
+            match = count;
+        }
+        count++;
+    }
+    if (match > -1){
+        return match;
+    }
+
+    // If no tables found returns -1
     return match;
 }
